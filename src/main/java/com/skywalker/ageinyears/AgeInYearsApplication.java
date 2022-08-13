212df0ef9becf033bf2185ec98b43c6a6da533d8
Ovie Udih <oviisaacs@gmail.com>
2022-08-13 14:29:54 +0100

    initial spring boot set up

---
+++ src/main/java/com/skywalker/ageinyears/AgeInYearsApplication.java
@@ -0,0 +1,13 @@
+package com.skywalker.ageinyears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeInYearsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgeInYearsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skywalker/ageinyears/AgeInYearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skywalker.ageinyears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeInYearsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
